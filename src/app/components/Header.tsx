1f1c8fa6719300f3c406683b0061568ec412a22b
mindset001 <shittumoshoodademola@gmail.com>
2025-10-23 15:13:36 +0100

    'neat'

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,3 @@
-
-
 import React from 'react';
 import Image from 'next/image';
 import { StarFilled, UserOutlined, QuestionCircleOutlined, ShoppingCartOutlined, SearchOutlined, DownOutlined } from '@ant-design/icons';
@@ -10,10 +8,10 @@ const Header = () => {
     <header className="w-full bg-white shadow-sm font-sans">
       <div className="flex items-center justify-between bg-gray-100 px-8 py-2 text-[15px]">
         <span className="text-orange-500 font-medium flex items-center">
-          <span className='bg-[orange] rounded-full p-[4px] mr-1'><StarFilled className="text-white" /></span> Sell on Jumia
+          <span className='bg-orange-500 rounded-full w-6 h-6 flex items-center justify-center mr-1'><StarFilled className="!text-white text-base" /></span> Sell on Jumia
         </span>
         <div className="flex items-center gap-5 text-gray-400 text-[15px]">
-          <span className="font-bold text-gray-900 text-lg tracking-widest">JUMIA <span className='bg-[orange] rounded-full p-[4px] mr-1'><StarFilled className="text-white" /></span> </span>
+          <span className="font-bold text-gray-900 text-lg tracking-widest flex"><span>JUMIA</span> <span className='bg-orange-500 rounded-full w-6 h-6 flex items-center justify-center mr-1'><StarFilled className="!text-white text-base" /></span> </span>
           <span className="opacity-60">PAY</span>
           <span className="opacity-60">DELIVERY</span>
         </div>
@@ -21,7 +19,7 @@ const Header = () => {
       </div>
       <div className="flex items-center justify-between px-8 pt-5 pb-2 bg-white">
         <div className="flex-none">
-          <span className="font-bold text-gray-900 text-4xl tracking-widest">JUMIA<span className='bg-[orange] rounded-full p-[4px] mr-1'><StarFilled className="text-white" /></span> </span>
+          <span className="font-bold text-gray-900 text-4xl tracking-widest flex"><span>JUMIA</span><span className='bg-orange-500 rounded-full w-10 h-10 flex items-center justify-center mr-1'><StarFilled className="!text-white text-[30px]" /></span> </span>
         </div>
        <div className='flex items-center'>
          <div className="flex items-center flex-1 mx-8 bg-white rounded-lg border">
